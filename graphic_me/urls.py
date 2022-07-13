3b073554561c7f687a43b0af8ebe2473e402cd7b
rovan89 <devireland89@gmail.com>
2022-07-13 18:00:43 +0000

    Link products urls.py to main app

--- graphic_me/urls.py
+++ graphic_me/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('products/', include('products.urls')),
     path('', include('home.urls')),
 ]
