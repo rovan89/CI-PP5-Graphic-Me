e5e82a1bf5af2398bfae964af69ad819cf3be6dd
rovan89 <devireland89@gmail.com>
2022-07-14 15:47:40 +0000

    Add portfolio function based view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def portfolio(request):
+    """ This view returns the portfolio page """
+
+    return render(request, 'products/portfolio.html')
