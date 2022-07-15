7002cd10b0ea8d07585089ee072408803086fa58
rovan89 <devireland89@gmail.com>
2022-07-15 10:09:21 +0000

    Register models to admin page and add a list display for Products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
