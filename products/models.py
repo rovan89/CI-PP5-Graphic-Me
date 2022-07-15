2e678629459682252e25e40c2fc493c81331d2fe
rovan89 <devireland89@gmail.com>
2022-07-15 14:03:01 +0000

    Add ID and image url fields to product model

--- products/models.py
+++ products/models.py
@@ -16,11 +16,13 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
+    id = models.BigAutoField(primary_key=True)
     category = models.ForeignKey('Category',null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=250, blank=True)
     description = models.TextField()
     size = models.BooleanField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     number_of_concepts = models.IntegerField(default=1)
     number_of_designs = models.IntegerField(default=1)
