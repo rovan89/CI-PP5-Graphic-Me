810b67a3561ce6e3b67c0062285c7180954c1dc5
rovan89 <devireland89@gmail.com>
2022-07-15 13:45:25 +0000

    Add context to portfolio function

--- products/views.py
+++ products/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def portfolio(request):
-    """ This view returns the portfolio page """
+    """ This view returns the portfolio page, including sorting and search quries"""
 
-    return render(request, 'products/portfolio.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/portfolio.html', context)
