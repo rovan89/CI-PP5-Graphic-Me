b6f402856caa8f1263623cac08581c58a64ccb5b
rovan89 <devireland89@gmail.com>
2022-07-15 14:01:35 +0000

    Add ID to Product list display

--- products/admin.py
+++ products/admin.py
@@ -4,11 +4,13 @@ from .models import Category, Product
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'name',
         'category',
         'price',
         'image',
     )
+
 
 admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
