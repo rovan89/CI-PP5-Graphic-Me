ebdc71bee1136688ca2d2bff741475caefe1cb84
rovan89 <devireland89@gmail.com>
2022-07-25 16:07:19 +0000

    Add bag_contents function and discount logic

---
+++ bag/contexts.py
@@ -0,0 +1,23 @@
+from decimal import Decimal
+from django.conf import settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total > settings.DISCOUNT_THESHOLD:
+        discount = total * Decimal(settings.DISCOUNT_PERCENTAGE / 100)
+
+    grand_total = discount + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'discount': discount,
+        'grand_total': grand_total, 
+    }
+
+    return context
