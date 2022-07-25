089fb4af1eb3825175bd77fc683ad3b5acdcf425
rovan89 <devireland89@gmail.com>
2022-07-25 13:35:52 +0000

    Add price field to Category model

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
         
     name = models.CharField(max_length=250, blank=True)
     friendly_name = models.CharField(max_length=250, null=False, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
@@ -17,13 +18,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
-    category = models.ForeignKey('Category',null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=250, blank=True)
     description = models.TextField()
     size = models.BooleanField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    number_of_concepts = models.IntegerField(default=1)
-    number_of_designs = models.IntegerField(default=1)
     created_on = models.DateTimeField(auto_now_add=True)
