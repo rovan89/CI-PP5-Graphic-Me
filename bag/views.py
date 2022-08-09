661a7813a41e0bdc5a5e10b03e11ebc83fcdd743
rovan89 <devireland89@gmail.com>
2022-08-09 15:02:06 +0000

    Add add_to_bag view

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from products.models import Product
+from shop.models import Order
+
 
 def view_bag(request):
     """ A view to return the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request):
+
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+    
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+
+    return redirect(redirect_url)
