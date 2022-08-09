22718b8cc2b3510baa3b7432ca9bf3738fbcfff6
rovan89 <devireland89@gmail.com>
2022-08-09 14:50:20 +0000

    Add OrderAdmin fields and list display

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Order, OrderDesignItem
 
-# Register your models here.
+class OrderDesignItemAdminInline(admin.TabularInline):
+    model = OrderDesignItem
+    readonly_fields = ('design_order_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderDesignItemAdminInline,)
+
+    readonly_fields = (
+        'order_number',
+        'category',
+        'number_of_concepts',
+        'price',
+        'created_on',
+    )
+    
+    list_display = (
+        'id',
+        'order_number',
+        'user',
+        'category',
+        'number_of_concepts',
+        'price',
+        'created_on',
+    )
+
+    ordering =('-created_on',)
+
+admin.site.register(Order, OrderAdmin)
