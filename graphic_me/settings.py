4ab96811ea958e00f2a43e4008570d9b385336ad
rovan89 <devireland89@gmail.com>
2022-08-09 15:01:33 +0000

    Add portfolio and crispy_forms to Installed apps and crispy forms settings to settings.py

--- graphic_me/settings.py
+++ graphic_me/settings.py
@@ -43,8 +43,12 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'portfolio',
     'shop',
     'bag',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'graphic_me.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +81,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
