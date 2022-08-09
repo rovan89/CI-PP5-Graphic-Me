9c89c79a6731c8842c47cb892b5aa50121e2c4be
rovan89 <devireland89@gmail.com>
2022-08-09 14:56:12 +0000

    Add product and product by id paths to urlpatterns

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('portfolio/', views.portfolio, name="portfolio"),
-    path('portfolio/<int:product_id>', views.portfolio_detail, name="portfolio_detail"),
+    path('product/', views.all_products, name="product"),
+    path('product/<int:product_id>', views.portfolio_detail, name="product_detail"),
 ]
