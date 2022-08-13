4180373393808e076b10a14862aead7c4f44b4d5
rovan89 <devireland89@gmail.com>
2022-08-13 11:57:40 +0000

    Add function to calculate the users order

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,25 +1,59 @@
 from decimal import Decimal
 from django.conf import settings
+from products.models import Category
 
 def bag_contents(request):
 
+    """ 
+    This function calculates the users order,
+    including a discount if the user reaches a certain threshold
+    
+     """
+
     bag_items = []
     total = 0
-    product_count = 0
+    category_price = 0
+    category_name = None
+    concept_price = 100
+    bag = request.session.get('bag', {})
+    
+    for item in bag.items():
+        category_item = Category.objects.all().values()
+        if 'category' in item:
+            item = int(item[1])
+            for i in category_item:
+                if(item == i['id']):
+                    print('Success: ', item, '==', i)
+                    category_price = i['price']
+                    category_name = i['name']
+                    concept_bag = int(bag['number_of_concepts'])
+                    concept_price = concept_price * concept_bag
 
-    if total > settings.DISCOUNT_THESHOLD:
+    total = concept_price + category_price
+
+    if total > settings.DISCOUNT_THRESHOLD:
         discount = total * Decimal(settings.DISCOUNT_PERCENTAGE / 100)
     else:
-        discount = 0
+        total = 0
 
-    grand_total = discount + total
+    bag_items.append({
+        'bag': bag,
+        'concept_price': concept_price,
+        'category_price': category_price,
+        'category_name': category_name,
+        'total': total,
+        
+    })
+
+    grand_total = total - discount
+    print('GRAND TOTAL: ', discount)
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
         'discount': discount,
         'grand_total': grand_total,
+
     }
 
     return context
