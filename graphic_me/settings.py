aea3fbb0f25cfc03f47f4e64efbbac337d45b789
rovan89 <devireland89@gmail.com>
2022-08-15 08:38:55 +0000

    Deployment part 2 fix errors

--- graphic_me/settings.py
+++ graphic_me/settings.py
@@ -172,7 +172,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-DISABLE_COLLECTSTATIC=1
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
