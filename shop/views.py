ca61a48b80d4dcef909c2495873a0829c9b80674
rovan89 <devireland89@gmail.com>
2023-01-19 14:00:01 +0000

    Add OrderForm to view

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
-from .models import Order
+from django.shortcuts import render, redirect
+from .models import Order, OrderDesignItem
 from .forms import OrderForm
+from django.contrib import messages
 
 def shop_form(request):
     """ This view returns the shop page with form for customers to detail what they want"""
-    
     form = OrderForm()
-    
+    form = OrderForm(request.POST, request.FILES)
+    print("|| shop/views.py || REQUEST", request.POST)
+    print("|| shop/views.py || REQUEST", request.POST)
     context = {
         'form': form
     }
 
+    if request.method == 'POST':
+        form = OrderForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            user_form = form.save(commit=False)
+            user_form.user = request.user
+            user_form.save()
+            return redirect('home')
     return render(request, 'shop/shop_main.html', context)
 
 
