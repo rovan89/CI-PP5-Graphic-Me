f19f200af5eb7de255f20aaca5f6dbce663c978f
rovan89 <devireland89@gmail.com>
2023-01-19 17:59:43 +0000

    Add functionality for user to add items to bag and recive a total

--- bag/contexts.py
+++ bag/contexts.py
@@ -24,17 +24,27 @@ def bag_contents(request):
     concept_price = 0
     bag = request.session.get('bag', {})
     
-    print(bag)
-    order_item = Order.objects.all().values()
+    # print(bag)
+    order_item = Order.objects.filter(user=request.user)
+    print(order_item)
+    # print("| contexts.py | ORDER ITEM: ", order_item)
+    order_user = Order.objects.filter(user=request.user.id)
+
+                
+    # print("| contexts.py | ORDER USER: ", order_user)
     category = Category.objects.all().values()
     current_user = request.user.id
+
     order_user_id = None
 
         
     for ordered_item in order_item:
-        order_user_id = ordered_item.get('user_id')
+        print(type(ordered_item))
+        order_user_id = ordered_item.user_id
+        print("\n| contexts.py | ORDER_USER_ID: ", order_user_id)
 
-        ordered_category_id = ordered_item.get('category_id')
+        ordered_category_id = ordered_item.user_id
+        print("CATEGORY ORDER ID: ",  ordered_category_id)
         for i in category:
             cat_id = i.get('id')
             cat_name = i.get('friendly_name')
@@ -43,7 +53,7 @@ def bag_contents(request):
                 category_price = i.get('price')
                 
         if current_user == order_user_id:
-            concept_bag = int(ordered_item.get('number_of_concepts'))
+            concept_bag = ordered_item.number_of_concepts
             concept_price = 100 * concept_bag
 
             total = concept_price + category_price
@@ -65,9 +75,25 @@ def bag_contents(request):
             })
 
     for i in bag_items:
+        # print("\n| contexts.py | ORDER USER I: ", i.get('total'))
+
         sub_total = i.get('total')
         if current_user == order_user_id:
             cart_total = cart_total + sub_total
+            # print("\n| contexts.py | ORDER USER FOR LOOP: ", cart_total)
+
+
+           #for i in order_user:
+            #    for x in order_item:
+            #        i = str(i)
+            #        x = x.get("order_number")
+            #        x = str(x)
+             #       print("\n| contexts.py | ORDER USER FOR LOOP: ", i, type(i))
+             #       print("| contexts.py | ORDER_ITEM USER FOR: ", x, type(x))
+             #       print("_________________________________________________________________________")
+             #       if x == i:
+             #           print("WORKING")
+                        
 
     if cart_total > settings.DISCOUNT_THRESHOLD:
         discount = cart_total * Decimal(settings.DISCOUNT_PERCENTAGE / 100)
@@ -87,3 +113,4 @@ def bag_contents(request):
     }
 
     return context
+
