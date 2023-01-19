f2f238302fb0836bf7d61786cbc2336480d0ae9b
rovan89 <devireland89@gmail.com>
2023-01-19 14:00:32 +0000

    Fix Concept Choices from str to int

--- shop/models.py
+++ shop/models.py
@@ -8,11 +8,11 @@ from django.contrib.auth.models import User
 from products.models import Category, Product
 
 CONCEPT_CHOICES = [
-    ('1', '1'),
-    ('2', '2'),
-    ('3', '3'),
-    ('4', '4'),
-    ('5', '5'),
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
 ]
 
 class Order(models.Model):
@@ -20,14 +20,15 @@ class Order(models.Model):
 
     id = models.BigAutoField(primary_key=True)
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_order")
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     created_on = models.DateTimeField(auto_now_add=True)
     description = models.TextField(null=True, blank=True,)
     image = models.ImageField(null=True, blank=True)
-    number_of_concepts = models.IntegerField(choices=CONCEPT_CHOICES )
+    number_of_concepts = models.IntegerField(choices=CONCEPT_CHOICES)
     price = models.DecimalField(max_digits=8, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=8, decimal_places=2, null=False, default=0)
+    paid = models.BooleanField(default=False)
 
     def _generate_order_number(self):
         """ Generate a random, unique order number using UUID """
@@ -46,13 +47,28 @@ class Order(models.Model):
 
 class OrderDesignItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='designitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, blank=True, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, null=False, blank=False, on_delete=models.CASCADE)
     design_order_total = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False, editable=False)
+
+    def _generate_item_number(self):
+        """ Generate a random, unique order number using UUID """
+
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """" Override original save method and set order number """
+        if not self.item_number:
+            self.item_number = self._generate_item_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.item_number
 
     def save(self, *args, **kwargs):
         self.design_order_total = self.category.price
         super().save(*args, **kwargs) 
 
-    def __str__(self):
-        return f'ID {self.product.id} on order {self.order.order_number}'
+    # def __str__(self):
+    #   return f'ID {self.product.id} on order {self.order.order_number}'
+
