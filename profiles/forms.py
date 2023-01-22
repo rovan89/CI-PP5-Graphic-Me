f9aa6a080385fbd37ee6064dce389cf3ff4c94dd
rovan89 <devireland89@gmail.com>
2023-01-22 18:31:09 +0000

    Add user profile form

---
+++ profiles/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm
+from django import forms
+
+from .models import UserProfile
+
+
+
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+
+        exclude = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            
+        }
