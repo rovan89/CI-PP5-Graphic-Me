fb8734741d9fa73cb2e2b1286486b41c751ce2a0
rovan89 <devireland89@gmail.com>
2023-01-22 16:53:16 +0000

    Add view to render profile.html template

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def profile(request):
+    """ Renders user profile"""
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
