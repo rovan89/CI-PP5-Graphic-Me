704cc09e872d84114121e8c25f6e9674a119e9c6
rovan89 <devireland89@gmail.com>
2023-01-26 10:01:20 +0000

    Add success message to shop_form

--- shop/views.py
+++ shop/views.py
@@ -5,11 +5,15 @@ from django.contrib import messages
 
 def shop_form(request):
     """ This view returns the shop page with form for customers to detail what they want"""
+    current_user = request.user.id
     form = OrderForm()
-    form = OrderForm(request.POST, request.FILES)
+    form = OrderForm( request.POST, request.FILES)
     print("|| shop/views.py || REQUEST", request.POST)
     print("|| shop/views.py || REQUEST", request.POST)
+
+   
     context = {
+        'current_user': current_user,
         'form': form
     }
 
@@ -19,24 +23,11 @@ def shop_form(request):
         if form.is_valid():
             user_form = form.save(commit=False)
             user_form.user = request.user
+            # print("|| shop/views.py || REQUEST CURRENT USER",  user_form.user)
+
             user_form.save()
+            messages.success(request, f'Order successfully added to bag')
             return redirect('home')
     return render(request, 'shop/shop_main.html', context)
 
-
-def chekout(request, order_number):
-
-    order_number = request.POST.get('order_number')
-    category = request.POST.get('category')
-    number_of_concepts = int(request.POST.get('number_of_concepts'))
-    price = request.POST.get('price')
-
-    context = {
-        'order_number': order_number,
-        'category': category,
-        'number_of_concepts': number_of_concepts,
-        'price': price,
-    }
-
-    return render(request, 'shop/checkout.html', context)
     
