cf7395d8e135cbeeb837f2ce7263469265344efd
rovan89 <devireland89@gmail.com>
2023-01-26 10:16:38 +0000

    Add SECRET_KEY

--- graphic_me/settings.py
+++ graphic_me/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-#SECRET_KEY = 'django-insecure--eyi2&74v&-1s&lr(k)2lo+rreh4$hnz&2r(fhl(6906k7$l^='
+#SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure--eyi2&74v&-1s&lr(k)2lo+rreh4$hnz&2r(fhl(6906k7$l^='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
