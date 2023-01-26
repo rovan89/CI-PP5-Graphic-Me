a0f34d258fa7268b3b85b317dd230f6147cbeb17
rovan89 <devireland89@gmail.com>
2023-01-26 10:11:39 +0000

    Add ready method to BagConfig

--- bag/apps.py
+++ bag/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BagConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bag'
+
+    def ready(self):
+        import bag.signals
