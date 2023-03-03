1ef81ad16f24e0a4687ad583ee88a84ae01d1293
rovan89 <devireland89@gmail.com>
2023-03-03 03:51:16 +0000

    Fix line is too long and add blank lines between classes

--- products/models.py
+++ products/models.py
@@ -5,20 +5,22 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
-        
+
     name = models.CharField(max_length=250, blank=True)
     friendly_name = models.CharField(max_length=250, null=False, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
-    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        Category, null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=250, blank=True)
     description = models.TextField()
     size = models.BooleanField()
