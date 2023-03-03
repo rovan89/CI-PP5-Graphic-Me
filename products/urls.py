bf7e553bd72e818d39a87772961e401328dbb3e7
rovan89 <devireland89@gmail.com>
2023-03-03 03:50:27 +0000

    Fix line is to long as per validation

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('product/', views.all_products, name="product"),
-    path('product/<int:product_id>', views.portfolio_detail, name="product_detail"),
+    path('product/<int:product_id>',
+         views.portfolio_detail, name="product_detail"),
 ]
