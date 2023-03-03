5c2ab521336ee6b220a7e88e51d4a38a773e085e
rovan89 <devireland89@gmail.com>
2023-03-03 03:52:37 +0000

    Fix line is too long as per validation

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.full_portfolio, name="portfolio"),
-    path('portfolio/<int:product_id>', views.portfolio_detail, name="portfolio_detail"),
+    path(
+        'portfolio/<int:product_id>',
+        views.portfolio_detail, name="portfolio_detail"),
 ]
