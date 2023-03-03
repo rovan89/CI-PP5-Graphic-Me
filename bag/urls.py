9f395962016c67bfaa5168b1a002d8bd67a5e150
rovan89 <devireland89@gmail.com>
2023-03-03 03:54:17 +0000

    Fix line is too long

--- bag/urls.py
+++ bag/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('add/', views.add_to_bag, name="add_to_bag"),
-    path('delete/<ordered_item>/', views.remove_from_bag, name="remove_from_bag"),
-    path('edit/<item_id>/', views.edit_item, name="edit"),
+    path('delete/<ordered_item>/',
+         views.remove_from_bag, name="remove_from_bag"),
+    path('edit/<item_id>/',
+         views.edit_item, name="edit"),
     path('checkout/', views.checkout, name="checkout"),
     path('checkout_success/', views.checkout_success, name="checkout_success"),
     path('', views.view_bag, name="view_bag"),
