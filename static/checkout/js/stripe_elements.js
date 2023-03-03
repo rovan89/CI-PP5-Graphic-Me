9e9d31ba17587a20b495bda2b3479700403416c9
rovan89 <devireland89@gmail.com>
2023-03-03 00:24:25 +0000

    Change name var to stripePublicKey

--- static/checkout/js/stripe_elements.js
+++ static/checkout/js/stripe_elements.js
@@ -1,7 +1,7 @@
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1,-1);
 var client_secret = $('#id_client_secret').text().slice(1,-1);
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
 var style = {
