3941a4476b57362d593623ee18870fd716297b85
rovan89 <devireland89@gmail.com>
2023-03-03 03:54:36 +0000

    Add blank lines between blocks of code

--- bag/signals.py
+++ bag/signals.py
@@ -2,12 +2,14 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from shop.models import OrderDesignItem
 
+
 @receiver(post_save, sender=OrderDesignItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
     instance.order.update_total()
+
 
 @receiver(post_delete, sender=OrderDesignItem)
 def delete_on_save(sender, instance, **kwargs):
